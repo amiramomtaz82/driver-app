c96570488c4416f9be3e8f2091ffd56a44532762
Amira Momtaz <amiraomtaz82@gmail.com>
2026-09-15 08:23:37 +0300

    update rout config

--- lib/core/go_routes/app_routers.dart
+++ lib/core/go_routes/app_routers.dart
@@ -1,23 +1,14 @@
-// import 'package:driver_app/core/go_routes/routes_names.dart';
-// import 'package:flutter/cupertino.dart';
-// import 'package:go_router/go_router.dart';
-//
-// class AppRouter {
-//   AppRouter._();
-//
-//   static final GlobalKey<NavigatorState> navigatorKey =
-//   GlobalKey<NavigatorState>();
-//
-//   static final GoRouter router = GoRouter(
-//     navigatorKey: navigatorKey,
-//     initialLocation: AppRoutes.login,
-//
-//     routes: [
-//     GoRoute(
-//     path: AppRoutes.login,
-//
-//       builder: (context, state) =>  const LoginView(),
-//
-//   ),]
-//   );
-// }
+import 'package:driver_app/core/go_routes/routes_names.dart';
+import 'package:go_router/go_router.dart';
+
+import '../../features/auth/presentation/login/login_view.dart';
+final GoRouter appRouter = GoRouter(
+  initialLocation: AppRoutes.login,
+  routes: [
+  GoRoute(
+  path: AppRoutes.login,
+  name: AppRoutes.login,
+  builder: (context, state) => const LoginView(),
+),
+]
+);
--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,20 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 
+
 import 'config/di/di.dart';
 
+import 'core/go_routes/app_routers.dart';
 void main() async{
-  configureDependencies();
+  WidgetsFlutterBinding.ensureInitialized();
+
   await EasyLocalization.ensureInitialized();
+  configureDependencies();
   runApp(EasyLocalization(
     supportedLocales: const [Locale('en'), Locale('ar')],
     path: 'assets/translations',
     fallbackLocale: const Locale('en'),
-    child: const MyApp(),
+    child: MyApp( ),
   ),
   );
 }
@@ -21,7 +25,11 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return  MaterialApp.router(
+      routerConfig: appRouter,
+      localizationsDelegates: context.localizationDelegates,
+      supportedLocales: context.supportedLocales,
+      locale: context.locale,
     );
   }
 }
