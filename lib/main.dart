5459d3c956b9787e8005d47ad3e19561e1ce0be9
Amira Momtaz <amiraomtaz82@gmail.com>
2026-09-17 23:01:24 +0300

    fix en.json and in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,21 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 
+
 import 'config/di/di.dart';
 
+import 'core/app_theme/app_theme.dart';
+import 'core/go_routes/app_routers.dart';
 void main() async{
-  configureDependencies();
+  WidgetsFlutterBinding.ensureInitialized();
+
   await EasyLocalization.ensureInitialized();
+  configureDependencies();
   runApp(EasyLocalization(
     supportedLocales: const [Locale('en'), Locale('ar')],
     path: 'assets/translations',
     fallbackLocale: const Locale('en'),
-    child: const MyApp(),
+    child: MyApp( ),
   ),
   );
 }
@@ -21,7 +26,14 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return  MaterialApp.router(
+      routerConfig: appRouter,
+      localizationsDelegates: context.localizationDelegates,
+      supportedLocales: context.supportedLocales,
+      locale: context.locale,
+      theme: AppTheme.lightTheme,
+      darkTheme: AppTheme.darkTheme,
+      themeMode: ThemeMode.system,
     );
   }
 }
