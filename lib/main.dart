4f3a29a110656fe5500461698ee691c90379e38e
Amira Momtaz <amiraomtaz82@gmail.com>
2026-09-17 23:01:24 +0300

    fix en.json and in main

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 
 import 'config/di/di.dart';
 
+import 'core/app_theme/app_theme.dart';
 import 'core/go_routes/app_routers.dart';
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
@@ -30,6 +31,9 @@ class MyApp extends StatelessWidget {
       localizationsDelegates: context.localizationDelegates,
       supportedLocales: context.supportedLocales,
       locale: context.locale,
+      theme: AppTheme.lightTheme,
+      darkTheme: AppTheme.darkTheme,
+      themeMode: ThemeMode.system,
     );
   }
 }
